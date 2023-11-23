8a97cbbeb1ebeb9d1be6f9959afbf0ef74ad77c1
daniel <daniel@gmail.com>
2023-11-23 13:48:39 +0000

    localStorage

--- js/script.js
+++ js/script.js
@@ -1,40 +1,62 @@
 class Formulario {
     comprobantes = {
-        "nombre": /^[A-Z][a-z]+/,
-        "apellidos": /^[A-Z][a-z]+\s[A-Z][a-z]+/,
-        "dni": /^[A-Z][1-9]{7}[A-Z]{1}$|^[1-9]{8}[A-Z]{1}$/,
+        "nombre": /^[A-Z][a-z]+$/,
+        "apellidos": /^[A-Z][a-z]+\s[A-Z][a-z]+$/,
+        "dni": /^[a-zA-z][1-9]{7}[A-Z]{1}$|^[1-9]{8}[A-Z]{1}$/,
         "nacimiento": {
-            test: function (texto) {
-                if(!/^\d{2}\/\d{2}\/\d{4}$/.test(texto)){
-                    return false}
-                texto = texto.split("/")
-                let dia = new Date(texto[1] + "/" + texto[0] + "/" + texto[2])
-                if(Boolean(dia.getDate()) && (dia.getDate() == Number(texto[0]))){
-                    return dia < new Date()
-                }else{
-                    return false
+            test:
+                function (texto) {
+                    if (!/^\d{2}\/\d{2}\/\d{4}$/.test(texto)) {
+                        return false
+                    }
+                    let textoLista = texto.split("/")
+                    let fecha = new Date(textoLista[1] + "/" + textoLista[0] + "/" + textoLista[2])
+                    if (fecha.getDate() == Number(textoLista[0])) {
+                        return fecha < new Date()
+                    } else {
+                        return false
+                    }
                 }
-            }
         },
-        "postal": /^[1-9]{5}$/,
+        "postal": /^[0-9]{5}$/,
         "email": /^[A-z.1-9]+@[A-z.1-9]+\.[A-z]{1,3}$/,
         "fijo": /^[89][0-9]{8}$/,
         "movil": /^[67][0-9]{8}$/,
         "iban": /^ES[0-9]{22}$/,
         "credito": /^[0-9]{12}$/,
-        "password": /(?=.*[0-9])(?=.*[A-z])(?=.*[!-\/:-@\[-_\{-~]).{6,}/,
-        "password_repeat": /(?=.*[0-9])(?=.*[A-z])(?=.*[!-\/:-@\[-_\{-~]).{6,}/
+
+        "password": {
+            test:
+                function (texto) {
+                    this.comprobarCampo(document.getElementById("password_repeat").value, "password_repeat")
+                    return /(?=.*[0-9])(?=.*[A-z])(?=.*[!-\/:-@\[-_\{-~]).{6,}/.test(texto)
+                }.bind(this)
+        },
+
+        "password_repeat": {
+            test:
+                function (texto) {
+                    if (!/(?=.*[0-9])(?=.*[A-z])(?=.*[!-\/:-@\[-_\{-~]).{6,}/.test(texto)) {
+                        return false
+                    } else {
+                        return document.getElementById("password").value == texto
+                    }
+                }
+        }
     }
 
     constructor() {
+
         let hijos = [...document.getElementById("formulario").querySelectorAll("input")]
         for (let hijo of hijos) {
             hijo.addEventListener("input", function (e) {
                 this.comprobarCampo(e.target.value, e.target.id)
             }.bind(this))
         }
+
+        document.getElementById("guardar").onclick = function () { this.guardar() }.bind(this)
+        document.getElementById("recuperar").onclick = function () { this.recuperar() }.bind(this)
     }
-    // TODO: generar objeto para test password y password_repeat
 
     comprobarCampo(texto, id_formulario) {
         if (this.comprobantes[id_formulario].test(texto)) {
@@ -44,13 +66,28 @@ class Formulario {
             document.getElementById(id_formulario).classList.add("no_valido")
             document.getElementById(id_formulario).classList.remove("valido")
         }
-        if (["password", "password_repeat"].includes(id_formulario)) {
-            if (document.getElementById("password_repeat").value != document.getElementById("password").value) {
-                document.getElementById("password_repeat").classList.remove("valido")
-                document.getElementById("password_repeat").classList.add("no_valido")
-            } else {
-                document.getElementById("password_repeat").classList.remove("no_valido")
-                document.getElementById("password_repeat").classList.add("valido")
+    }
+
+
+    guardar() {
+        let hijos = [...document.getElementById("formulario").querySelectorAll("input")]
+        let comprobacionTodos = true
+        hijos.forEach(function (hijo) {
+            if (!hijo.classList.contains("valido")) {
+                comprobacionTodos = false
+            }
+        })
+        hijos.forEach(function (hijo) {
+            localStorage[hijo.id] = hijo.value
+        })
+    }
+
+    recuperar() {
+        if (localStorage) {
+            for (let i = 0; i < localStorage.length; i++) {
+                let item = localStorage.key(i)
+                document.getElementById(item).value = localStorage[item]
+                this.comprobarCampo(localStorage[item], item)
             }
         }
     }
